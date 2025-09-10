a049392e143a37be32ad85cc61930bd049bc4dd3
Namit Gupta 25BDE0068 <namit.gupta2025@vitstudent.ac.in>
2025-09-10 23:11:38 +0000

    why are you not ding it in one go?

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,13 +20,13 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center justify-between">
+      <div className="container flex h-14 items-center">
         <div className="flex items-center">
           <Link href="/dashboard" className="flex items-center space-x-2">
             <Logo size="text-3xl" />
           </Link>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="flex flex-1 items-center justify-end space-x-4">
            <Avatar>
             <AvatarFallback>{initial}</AvatarFallback>
           </Avatar>
